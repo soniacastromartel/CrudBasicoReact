6cabfe33639367d9fbbf8da43678034a92a7beea
Sonia Castro <soniacastromartel@gmail.com>
2021-08-02 12:06:28 +0100

    1.4 Showing the list of names

--- crud-basico/src/components/Listadonombres.jsx
+++ crud-basico/src/components/Listadonombres.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import uniqid from 'uniqid';
+import uniqid from "uniqid";
 
 export const Listadonombres = () => {
   const [nombre, setNombre] = useState("");
-  const [listaNombres, setListaNombres] = useState([])
+  const [listaNombres, setListaNombres] = useState([]);
 
-const addNombre = (e) =>{
-    e.preventDefault()
-    const nuevoNombre= {
-        id: uniqid(),
-        tituloNombre: nombre
-    }
-    setListaNombres([...listaNombres, nuevoNombre])
-}
+  const addNombre = (e) => {
+    e.preventDefault();
+    const nuevoNombre = {
+      id: uniqid(),
+      tituloNombre: nombre,
+    };
+    setListaNombres([...listaNombres, nuevoNombre]);
+  };
 
   return (
     <div className="container">
@@ -20,10 +20,17 @@ const addNombre = (e) =>{
       <div className="row">
         <div className="col">
           <h2>Listado de Nombres</h2>
+          <ul className="list-group">
+            {listaNombres.map((item) => (
+              <li key="{item.id}" className="list-group-item">
+                {item.tituloNombre}
+              </li>
+            ))}
+          </ul>
         </div>
         <div className="col">
           <h2>Formulario de Nombres</h2>
-          <form onSubmit= {(e) => addNombre (e)} className="form-group">
+          <form onSubmit={(e) => addNombre(e)} className="form-group">
             <input
               onChange={(e) => {
                 setNombre(e.target.value);
@@ -31,6 +38,7 @@ const addNombre = (e) =>{
               className="form-control mb-3"
               type="text"
               placeholder="Introduzca el nombre"
+              value= {nombre}
             />
             <input
               className="btn btn-info w-100"
